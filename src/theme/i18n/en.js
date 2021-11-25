28cce512c06662af5c5e887576d210c5424e185f
sunnywizard <95030950+sunnywizard@users.noreply.github.com>
2021-11-25 18:02:30 +0200

    Add files via upload

---
+++ src/theme/i18n/en.js
@@ -0,0 +1,6 @@
+export default {
+  'error.default': 'An error has occurred. Please contact technical support.',
+  'error.connect': 'Connection error. Repeat later or contact technical support.',
+  'pageNotFound.title': 'Page not found',
+  'logo.title': 'My education',
+};
